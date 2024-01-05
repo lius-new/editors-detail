a30834bf0126a9dabb7e2656360950546dd5ed2e
lius-new <liushui_new@126.com>
2024-01-05 15:30:38 +0800

    feat: wsl中添加wsl重启的快捷键

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -16,14 +16,14 @@ M.treesitter = {
   auto_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting=false,
+    additional_vim_regex_highlighting = false,
   },
   ident = { enable = true },
   rainbow = {
     enable = true,
     extended_mode = true,
     max_file_lines = nil,
-  }
+  },
 }
 
 M.mason = {
